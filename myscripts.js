c2a447124ce266cb9270f2a8c137b861d9c3e30b
takkorai <takkorai@gmail.com>
2017-02-07 14:50:11 -0500

    Added loop

---
+++ myscripts.js
@@ -0,0 +1,33 @@
+
+
+
+
+$(document).ready(function(){
+	var $square = '<div class="square"></div>'
+	var $grid = 16;
+
+
+	drawSquares();
+
+	$('.square').mouseenter(function(){
+		$(this).addClass("highlighted");
+	});
+
+    $(document).on('click','.button',function(){
+    	$grid = prompt("Enter the the number of columns/rows",16);
+    	while(isNaN($grid)){
+    		$grid = prompt("Plese Enter a valid column/row number",16);
+    	};
+		drawSquares();
+    });
+	function drawSquares(){
+		$('.square').remove('.square');
+		for(var i = 1; i<=$grid*$grid; i++){
+			$('.square').css("width",100/$grid+"%");
+			$('.square').css("height",100/$grid+"%");
+			$('.wrapper').append($square);
+		};
+	}
+});
+
+
